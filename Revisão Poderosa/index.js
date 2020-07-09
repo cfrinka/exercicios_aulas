e19b0c5cce875711ea2e2e320966e887f6695f33
Carlos Frinka Neto <c.frinkaneto@gmail.com>
2020-07-09 19:56:31 -0300

    atualizando

--- Revisão Poderosa/index.js
+++ Revisão Poderosa/index.js
@@ -1,26 +1,24 @@
-function validModel(model){
-    return {
-        'samsung': '1000,00 R$',
-        'apple' : '105,00 R$',
-        'xiaomi' : '150,00 R$',
-        'lg' : '200,00 R$',
-    }[model];
+let user = require('readline-sync');
+ 
+let player1 = user.question('Nome Jogador 1: \n');
+let p1 = [];
+let p2 = [];
+let resultado = [];
+ 
+for(let i = 0; i < 5; i++) {
+    p1[i] = user.question('Números Player 1 de 1 até 10: ');
 }
-function getPriceByModel() {
-    let result = document.getElementById('result');
-    let model = document.getElementById('model').value
-    model = String(model)
-    let price = 0
-
-    if (!model || !pricing){
-        return null;
-    }
-   
-    let pricing = validModel(model);
-
-    result.innerText = `O seu modelo é ${model} e custa ${pricing}`
-
-
-
-    
+ 
+let player2 = user.question('Nome jogador 2: ');
+ 
+for(let i = 0; i < 5; i++) {
+    p2[i] = user.question('Números Player 2 de 1 até 10: ');
 }
+ 
+ 
+for(let i = 0; i < 5; i++) {
+    if(!resultado.includes(p1[i])) resultado.push(p1[i]);
+    if(!resultado.includes(p2[i])) resultado.push(p2[i]);
+};
+ 
+console.log(resultado);
